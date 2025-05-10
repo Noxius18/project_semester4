020f97d911f54b18f3e0f73b956393808fc4157f
Noxius18 <akagiadam@gmail.com>
2025-05-10 14:06:12 +0700

    Dashboard #1

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,14 @@ Route::get('/', function () {
     return redirect()->route('auth.login');
 });
 
-Route::get('/dashboard', function() {
-    return view('dashboard');
-});
-
+// Login End-point
 Route::get('/login', [AuthController::class,'showLoginForm'])->name('auth.login');
 Route::post('/login', [AuthController::class,'login'])->name('login');
 Route::post('/logout', [AuthController::class,'logout'])->name('auth.logout');
+
+// Middleware Login admin end-point
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::get('/dashboard', function() {
+        return view('dashboard');
+    });
+});
